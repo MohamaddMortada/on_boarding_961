c47278fb9831d6d025d5598b6fbb1e219e6aaa04
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-02-07 14:57:06 +0200

    Creating MainButton

---
+++ lib/Widgets/main_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MainButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const MainButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 348,
+      height: 54,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Theme.of(context).primaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(45),
+          ),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
